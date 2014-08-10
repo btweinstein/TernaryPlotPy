712522234f6627bdda5a1724645688ab52ba3bf9
btweinstein <btweinstein@gmail.com>
2014-08-10 15:03:44 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TernaryPlotPy',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Bryan Weinstein',
+    author_email='bweinstein@seas.harvard.edu',
+    description=''
+)
